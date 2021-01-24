fd8d83881015b5fcb2c0351091b8822acdccc181
Maksim Kovalyov <makckovalyov@gmail.com>
2021-01-24 17:40:13 +0800

    v0.0.2

--- struct.go
+++ struct.go
@@ -3,15 +3,10 @@ package rabbitMqGolang
 import "github.com/streadway/amqp"
 
 type RabbitMQ struct {
-	Host string
-	Port int64
-	UserName string
-	Password string
+	Host               string
+	Port               int64
+	UserName           string
+	Password           string
 	rabbitMqConnection *amqp.Connection
-	isConnected bool
-}
-
-type Delivery struct {
-	amqp.Delivery
-	Name string
+	isConnected        bool
 }
